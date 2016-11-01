23e32a9cc08d05b54b3f9a4526fe74cf4f35a274
Julien Truffaut <truffaut.julien@gmail.com>
2016-11-01 09:18:45 +0000

    move overload App in optics file

--- src/main/scala/optic/overload/Optic.scala
+++ src/main/scala/optic/overload/Optic.scala
@@ -5,6 +5,8 @@ case class Traversal[A,B](desc : List[String]) {
   def compose[C](o: Traversal[B, C]): Traversal[A, C] = Traversal(desc ++ o.desc)
 
   def composeTraversal[C](o: Traversal[B, C]): Traversal[A, C] = compose(o)
+
+  override def toString: String = desc.mkString(",")
 }
 
 case class Lens[A,B](desc : List[String]) {
@@ -12,7 +14,27 @@ case class Lens[A,B](desc : List[String]) {
   def compose[C](o: Traversal[B, C]): Traversal[A, C] = Traversal(desc ++ o.desc)
 
   def composeTraversal[C](o: Traversal[B, C]): Traversal[A, C] = compose(o)
+
+  override def toString: String = desc.mkString(",")
 }
 
+object Test extends App {
 
+  val foo: Lens[String, Int] = Lens(List("foo"))
+  val bar: Traversal[Int, String] = Traversal(List("bar"))
+
+  // Fully defined optics compose fine
+  val foobar = foo compose bar
+  println(foo compose bar compose foo)
+
+  val ages: Lens[String, List[Int]] = Lens(List("ages"))
+  def all[A]: Traversal[List[A], A] = Traversal(List("all"))
+
+  //  does not compile
+  //  println((ages compose all).desc)
+  //  but it works for the following:
+  println(ages compose all[Int])
+  println(ages composeTraversal all)
+
+}
 
